a166da5cf6b429225529bd3b215ebfa331b4558a
Yaroslav Semeniuk <yaroslavsemeniuk@MacBook-Pro-Yaroslav.local>
2022-05-21 13:53:31 +0300

    create controllers with dependent dto

---
+++ src/review/review.controller.ts
@@ -0,0 +1,19 @@
+import {
+  Body, Controller, Delete, Get, HttpCode, Param, Post
+} from '@nestjs/common';
+import { ReviewModel } from './review.model';
+
+@Controller('review')
+export class ReviewController {
+  @HttpCode(200)
+  @Post('create')
+  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+
+  @Delete(':id')
+  async delete(@Param(':id') id: string) {
+
+  }
+
+  @Get('byProduct/:productId')
+  async getByProduct(@Param('productId') productId: string) {}
+}
