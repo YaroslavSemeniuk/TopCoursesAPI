cb99584febf5fc02386d4af8a7b82ca6c623492b
Yaroslav Semeniuk <sem.yarik1@gmail.com>
2022-08-13 16:52:26 +0300

    create auth service & validation pipes

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,2 @@
+export const ALREADY_REGISTERED_ERROR =
+	'User with this email is already registered';
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,31 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+	BadRequestException,
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
+import { ALREADY_REGISTERED_ERROR } from './auth.constants';
 
 @Controller('auth')
 export class AuthController {
-  @Post('register')
-  async register(@Body() dto: AuthDto) {}
+	constructor(private readonly authService: AuthService) {}
 
-  @HttpCode(200)
-  @Post('login')
-  async login(@Body() dto: AuthDto) {}
+	@UsePipes(new ValidationPipe())
+	@Post('register')
+	async register(@Body() dto: AuthDto) {
+		const existUser = await this.authService.findUser(dto.login);
+		if (existUser) {
+			throw new BadRequestException(ALREADY_REGISTERED_ERROR);
+		}
+		return this.authService.createUser(dto);
+	}
+
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() dto: AuthDto) {}
 }
